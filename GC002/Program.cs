b3dc15dce4ca4916bb1a1ffe3eae36ef40da71d9
Artem <a.khimin@yandex.ru>
2021-08-21 22:45:51 +0300

    метод  Dispose

---
+++ GC002/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace GC002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+            Test2();
+
+            Test3();
+            Test4();
+
+
+            Console.ReadLine();
+        }
+
+        private static void Test()
+        {
+            using (Person p = new Person { Name = "Tom" })
+            {
+                // переменная p доступна только в блоке using
+                Console.WriteLine($"Некоторые действия с объектом Person. Получим его имя: {p.Name}");
+            }
+            Console.WriteLine("Конец метода Test");
+            Console.WriteLine();
+        }
+
+        private static void Test2()
+        {
+            using (Person tom = new Person { Name = "Tom" })
+            {
+                using (Person bob = new Person { Name = "Bob" })
+                {
+                    Console.WriteLine($"Person1: {tom.Name}    Person2: {bob.Name}");
+                }// вызов метода Dispose для объекта bob
+            } // вызов метода Dispose для объекта tom
+            Console.WriteLine("Конец метода Test4");
+            Console.WriteLine();
+        }
+
+        private static void Test3()
+        {
+            using (Person tom = new Person { Name = "Tom" })
+            using (Person bob = new Person { Name = "Bob" })
+            {
+                Console.WriteLine($"Person1: {tom.Name}    Person2: {bob.Name}");
+            } // вызов метода Dispose для объектов bob и tom
+            Console.WriteLine("Конец метода Test3");
+            Console.WriteLine();
+        }
+
+        private static void Test4()
+        {
+            using Person tom = new Person { Name = "Tom" };
+            using Person bob = new Person { Name = "Bob" };
+
+            Console.WriteLine($"Person1: {tom.Name}    Person2: {bob.Name}");
+
+            Console.WriteLine("Конец метода Test4");
+            Console.WriteLine();
+        } // вызов метода private static void Test()
+{
+    using Person tom = new Person { Name = "Tom" };
+    using Person bob = new Person { Name = "Bob" };
+
+Console.WriteLine($"Person1: {tom.Name}    Person2: {bob.Name}");
+
+Console.WriteLine("Конец метода Test");
+} // вызов метода Dispose для объектов bob и tom для объектов bob и tom
+
+    }
+    public class Person : IDisposable
+    {
+        public string Name { get; set; }
+
+        
+        public void Dispose()
+        {
+            Console.WriteLine("Disposed");
+
+            Console.WriteLine(Name);
+        }
+    }
+
+
+}
--- GarbageCollector/Program.cs
+++ GarbageCollector/Program.cs
@@ -10,6 +10,100 @@ namespace GarbageCollector
     {
         static void Main(string[] args)
         {
+            Test();
+
+            GC.Collect();
+            Console.ReadLine();
         }
+
+        private static void Test()
+        {
+            Country country = new Country();
+            country.x = 10;
+            country.y = 15;
+        }
+
+        private static void Test2()
+        {
+            Person p = null;
+            try
+            {
+                p = new Person();
+            }
+            finally
+            {
+                if (p != null)
+                {
+                    p.Dispose();
+                }
+            }
+        }
+    }
+    public class Person : IDisposable
+    {
+        public string Name { get; set; }
+        public void Dispose()
+        {
+            Console.Beep();
+            Console.WriteLine("Disposed");
+        }
+    }
+
+    public class SomeClass : IDisposable
+    {
+        private bool disposed = false;
+
+        // реализация интерфейса IDisposable.
+        public void Dispose()
+        {
+            Dispose(true);
+            // подавляем финализацию
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Освобождаем управляемые ресурсы
+                }
+                // освобождаем неуправляемые объекты
+                disposed = true;
+            }
+        }
+
+        // Деструктор
+        ~SomeClass()
+        {
+            Dispose(false);
+        }
+    }
+
+    public class Derived 
+    {
+        private bool IsDisposed = false;
+
+        protected  void Dispose(bool disposing)
+        {
+            if (IsDisposed) return;
+            if (disposing)
+            {
+                // Освобождение управляемых ресурсов
+            }
+            IsDisposed = true;
+            // Обращение к методу Dispose базового класса
+            Dispose(disposing);
+        }
+    }
+
+
+
+
+    class Country
+    {
+        public int x;
+        public int y;
     }
 }
